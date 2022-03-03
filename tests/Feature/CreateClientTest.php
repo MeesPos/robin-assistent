604210535c08086850ab7c15f27e3a95a577444f
MeesPos <meespostma_@hotmail.com>
2022-03-03 12:42:11 +0100

    Removed variable

--- tests/Feature/CreateClientTest.php
+++ tests/Feature/CreateClientTest.php
@@ -33,7 +33,7 @@ class CreateClientTest extends TestCase
     /** @test */
     public function it_sends_an_validation_error_when_an_required_input_is_not_sended_with_the_request()
     {
-        $this->post('/client', $data = [
+        $this->post('/client', [
             'first_name' => 'John',
             'last_name' => 'Doe',
             'birth_date' => Carbon::today()->subYears(30)->toDateString(),

6b15a7e57068dc9ae9f7866fe473eb8f26596b83
MeesPos <meespostma_@hotmail.com>
2022-03-03 12:41:18 +0100

    Added more tests

--- tests/Feature/CreateActivityTest.php
+++ tests/Feature/CreateActivityTest.php
@@ -13,15 +13,20 @@ use Tests\TestCase;
 
 class CreateActivityTest extends TestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->actingAs(User::factory()->create());
+
+        $this->client = Client::factory()->create(['user_id' => Auth::id()]);
+    }
+
     /** @test */
     public function it_can_make_an_activity()
     {
-        $this->actingAs(User::factory()->create());
-
-        $client = Client::factory()->create(['user_id' => Auth::id()]);
-
         $this->post('/activity', $data = [
-            'client_id' => $client->getKey(),
+            'client_id' => $this->client->getKey(),
             'name' => 'Test name',
             'start_date' => Carbon::now()->addDay()->toDateString(),
             'end_date' => Carbon::now()->addDays(2)->toDateString(),
@@ -35,5 +40,41 @@ class CreateActivityTest extends TestCase
         ])->assertCreated();
 
         $this->assertDatabaseHas('activities', $data);
+    }
+
+    /** @test */
+    public function it_sends_an_validation_error_when_an_required_input_is_not_sended_with_the_request()
+    {
+        $this->post('/activity', [
+            'client_id' => $this->client->getKey(),
+            'name' => 'Test name',
+            'start_date' => Carbon::now()->addDay()->toDateString(),
+            'end_date' => Carbon::now()->addDays(2)->toDateString(),
+            'start_time' => Carbon::now()->addHour()->toTimeString(),
+            'end_time' => '',
+            'days' => json_encode([
+                DaysEnum::MONDAY->value,
+                DaysEnum::WEDNESDAY->value,
+                DaysEnum::SATURDAY->value
+            ])
+        ])->assertSessionHasErrors();
+    }
+
+    /** @test */
+    public function it_sends_an_validation_error_when_an_date_input_is_not_an_date()
+    {
+        $this->post('/activity', [
+            'client_id' => $this->client->getKey(),
+            'name' => 'Test name',
+            'start_date' => Carbon::now()->addDay()->toDateString(),
+            'end_date' => Carbon::now()->addDays(2)->toTimeString(), // Time, not date
+            'start_time' => Carbon::now()->addHour()->toTimeString(),
+            'end_time' => Carbon::now()->addHours(2)->toTimeString(),
+            'days' => json_encode([
+                DaysEnum::MONDAY->value,
+                DaysEnum::WEDNESDAY->value,
+                DaysEnum::SATURDAY->value
+            ])
+        ])->assertSessionHasErrors();
     }
 }

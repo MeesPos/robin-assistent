10b1dc156a1a59c1a1fcc13037dd5ae5c7a50240
MeesPos <meespostma_@hotmail.com>
2022-03-09 11:37:46 +0100

    Added selectable activities

--- resources/js/Pages/Activities/Create.vue
+++ resources/js/Pages/Activities/Create.vue
@@ -1,19 +1,46 @@
 <template>
-    <div>
-        <button v-for="activity in activities" @click.prevent="goToStepsPage(activity)">
-            <img :src="activity.image"
-                 class="w-20 h-20"
-                 :alt="activity.name"
+    <form @submit.prevent="submit">
+        <div class="flex flex-row">
+            <div v-for="activity in activities"
+                 :class="active === activity.unique_key ? 'border' : ''"
             >
+                <input class="hidden"
+                       :id="'radio_' + activity.unique_key"
+                       type="radio"
+                       @click="setActive(activity.unique_key)"
+                       name="radio"
+                />
 
-            <h2 v-text="activity.name" />
-        </button>
-    </div>
+                <label class="cursor-pointer"
+                       :for="'radio_' + activity.unique_key"
+                >
+                    <img :src="activity.image"
+                         class="w-20 h-20"
+                         :alt="activity.name"
+                    >
+
+                    <h2 v-text="activity.name" />
+                </label>
+            </div>
+        </div>
+
+        <input type="submit"
+               value="Next"
+        />
+    </form>
 </template>
 
 <script>
+import AppLayout from "../../Layouts/AppLayout";
+
 export default {
     name: "Create",
+    layout: AppLayout,
+    data() {
+        return {
+            active: ''
+        }
+    },
     props: {
         activities: {
             type: Array,
@@ -26,6 +53,14 @@ export default {
     methods: {
         goToStepsPage(activity) {
             return this.$inertia.get(`/activity/steps/${activity.unique_key}/${this.client_id}`);
+        },
+        setActive(activity) {
+            this.active = activity;
+        },
+        submit() {
+            if (this.active !== '') {
+                return this.$inertia.get(`/activity/steps/${this.active}/${this.client_id}`);
+            }
         }
     }
 }
